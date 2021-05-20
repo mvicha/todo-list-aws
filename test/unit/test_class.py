c1829238632fea3278e1a784b40383672ecba55a
Marcos F. Villa <mvicha@gmail.com>
2021-05-20 14:46:05 +0000

    Reduce cc to class b

--- test/unit/test_class.py
+++ test/unit/test_class.py
@@ -7,85 +7,70 @@ import sys
 
 class remoteTableClass():
     def launchEvent(self, endpoint_url, action, params=None):
-        if not endpoint_url:
-            httpCode = 500
-            responseBody = {
-                'errorCode': 0x11,
-                'errorMsg': 'No endpoint url defined',
-            }
-        elif not action:
+        http = urllib3.PoolManager()
+
+        if action in ['create', 'update']:
+            if action == 'create':
+                method = 'POST'
+            else:
+                method = 'PUT'
+
+        elif action != 'delete':
+            method = 'GET'
+
+        else:
+            method = 'DELETE'
+
+        try:
+            r = http.request(
+                method,
+                endpoint_url,
+                body=params
+            )
+
+            if action in ["create", "update", "get", "list", "translate"]:
+                response = json.loads(r.data.decode('utf-8'))
+                try:
+                    if 'message' in response:
+                        httpCode = 500
+                        responseBody = {
+                            'Items': json.dumps({
+                                'errorCode': 0x01,
+                                'errorMsg': response['message']
+                            })
+                        }
+                    else:
+                        httpCode = 200
+                        responseBody = {
+                            'Items': json.dumps({
+                                'errorCode': 200,
+                                'errorMsg': response
+                            })
+                        }
+                except Exception as e:
+                    httpCode = 501
+                    responseBody = {
+                        'Items': json.dumps({
+                            'errorCode': 0x02,
+                            'errorMsg': e
+                        })
+                    }
+            else:
+                httpCode = 200
+                responseBody = {
+                    'Items': json.dumps({
+                        'errorCode': 0x00,
+                        'errorMsg': 'Todo deleted successfully'
+                    })
+                }
+        except Exception as e:
             httpCode = 500
             responseBody = {
                 'Items': json.dumps({
-                    'errorCode': 0x12,
-                    'errorMsg': 'No action defined'
+                    'errorCode': 0x03,
+                    'errorMsg': f'Something happened: {e}'
                 })
             }
-        else:
-            http = urllib3.PoolManager()
-
-            if action in ['create', 'update']:
-                if action == 'create':
-                    method = 'POST'
-                else:
-                    method = 'PUT'
-
-            elif action != 'delete':
-                method = 'GET'
-
-            else:
-                method = 'DELETE'
-
-            try:
-                r = http.request(
-                    method,
-                    endpoint_url,
-                    body=params
-                )
-
-                if action in ["create", "update", "get", "list", "translate"]:
-                    response = json.loads(r.data.decode('utf-8'))
-                    try:
-                        if 'message' in response:
-                            httpCode = 500
-                            responseBody = {
-                                'Items': json.dumps({
-                                    'errorCode': 0x01,
-                                    'errorMsg': response['message']
-                                })
-                            }
-                        else:
-                            httpCode = 200
-                            responseBody = {
-                                'Items': json.dumps({
-                                    'errorCode': 200,
-                                    'errorMsg': response
-                                })
-                            }
-                    except Exception as e:
-                        httpCode = 501
-                        responseBody = {
-                            'Items': json.dumps({
-                                'errorCode': 0x02,
-                                'errorMsg': e
-                            })
-                        }
-                else:
-                    httpCode = 200
-                    responseBody = {
-                        'Items': json.dumps({
-                            'errorCode': 0x00,
-                            'errorMsg': 'Todo deleted successfully'
-                        })
-                    }
-            except Exception as e:
-                httpCode = 500
-                responseBody = {
-                    'Items': json.dumps({
-                        'errorCode': 0x03,
-                        'errorMsg': f'Something happened: {e}'
-                    })
-                }
         response = {
             "statusCode": httpCode,
             "body": json.dumps(responseBody)
