4dae989b1810b230ebb01f6e9bd620cd8edb17e5
Marcos F. Villa <mvicha@gmail.com>
2021-05-20 15:48:50 +0000

    Add ENDPOINT_URL env var for pytest

--- test/run_final.sh
+++ test/run_final.sh
@@ -2,6 +2,8 @@
 
 export PATH=${HOME}/.local/bin:${PATH}
 
+export ENDPOINT_URL=$(aws cloudformation describe-stacks --stack-name todo-list-aws-staging --query 'Stacks[0].Outputs[?OutputKey==`todoListResourceApiUrl`].OutputValue' --output text)
+
 echo 'Run final testing'
 cd /opt/todo-list-aws/test/unit
 pytest test_create.py -vvv
