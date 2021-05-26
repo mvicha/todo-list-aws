dd716fa7164b4e1240609f5c8233d044ce4be7bf
Marcos F. Villa <mvicha@gmail.com>
2021-05-26 07:38:37 +0000

    Fix comparison in run_final.sh

--- test/run_final.sh
+++ test/run_final.sh
@@ -2,7 +2,7 @@
 
 export PATH=${HOME}/.local/bin:${PATH}
 
-if [ "${1}" == "local" ]; then
+if [ "${1}" = "local" ]; then
     export ENDPOINT_URL="http://localhost:8080/todos"
 else
     export ENDPOINT_URL=$(aws cloudformation describe-stacks --stack-name todo-list-aws-${1} --query 'Stacks[0].Outputs[?OutputKey==`todoListResourceApiUrl`].OutputValue' --output text)
